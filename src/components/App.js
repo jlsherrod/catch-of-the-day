6cacdfc333ed8b2b2ca06f2998cb4c6a49818951
James Sherrod <jlsherrod@gmail.com>
2022-02-20 14:35:12 -0600

    Make Order.js functional
    Make the order component show all fish added from state to order. render
    all fish added to order and total prices.

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,11 @@ class App extends React.Component {
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes })
   }
+  addToOrder = (key) => {
+    const order = { ...this.state.order }
+    order[key] = order[key] + 1 || 1
+    this.setState({ order })
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -25,11 +30,11 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map((key) => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     )
