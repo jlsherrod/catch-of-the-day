46ebda2f4b420b978f1bec8f612bd0e3d4e53a9d
James Sherrod <jlsherrod@gmail.com>
2022-04-03 09:32:36 -0500

    Add PropTypes to all components
    Import the PropTypes package and define each 'this.props.*' with the
    correct propTypes.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 // Write as a stateless component
 
@@ -17,5 +18,9 @@ const Header = (props) => (
     </h3>
   </header>
 )
+
+Header.propTypes = {
+  tagline: PropTypes.string.isRequired,
+}
 
 export default Header
